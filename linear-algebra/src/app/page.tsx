219d5b286f2d649b0886dc090ec2eabf08ca2ca6
User <u96464224@gmail.com>
2024-09-26 20:48:04 +0900

    不要な部分を削除

--- linear-algebra/src/app/page.tsx
+++ linear-algebra/src/app/page.tsx
@@ -13,52 +13,35 @@ enum Mode  {
 
 
 export default function Home() {
-  const [val, setVal] = useState(0);
   const [vectorDimension, setVectorDimension] = useState(0);
   const [rows, setRows] = useState<number>(0);
   const [cols, setCols] = useState<number>(0);
   const [isRight, setIsRight] = useState(false);
-  const [isMatrixMode, setIsMatrixMode] = useState(false);
   const [isInputComplete, setInputComplete] = useState(false);
   const [mode, setMode] = useState<Mode>(Mode.Vector);
 
   const handleMode = () => {
     setMode(currentMode => currentMode === Mode.Vector ? Mode.Matrix : Mode.Vector);
   }
-
-  const handleValueChange = useCallback((newValue: number) => {
-    setVal(newValue);
-    setInputComplete(true);
-  }, []);
-
-  const handleRowChange = useCallback((newValue: number) => {
+  const handleRowChange = (newValue: number) => {
     setRows(newValue);
-  }, []);
+  }
 
-  const handleColChange = useCallback((newValue: number) => {
+  const handleColChange = (newValue: number) => {
     setCols(newValue);
-  }, []);
+  }
 
-  const handleToggleRowOrColSwitch = useCallback(() => {
+  const handleToggleRowOrColSwitch = () => {
     setIsRight(prev => !prev);
-  }, []);
+  }
 
-  const handleInputMode = useCallback(() => {
-    setIsMatrixMode(prev => !prev);
-    setInputComplete(false);
-  }, []);
-
-  const handleVectorDimensionChange = useCallback((newValue: number) => {
+  const handleVectorDimensionChange = (newValue: number) => {
     setVectorDimension(newValue);
     setInputComplete(true);
-  }, []);
+  };
 
   return (
     <>
-      <h1>数値入力のテスト {val}</h1>
-      <h1>行の入力 {rows}</h1>
-      <h1>列の入力 {cols}</h1>
-      <h1>{isMatrixMode ? "matrix" : "vector"}</h1>
       <h1>{mode}</h1>
       <ToggleSwitch leftLabel="vector" rightLabel="matrix" onToggle={handleMode}/>
       {
